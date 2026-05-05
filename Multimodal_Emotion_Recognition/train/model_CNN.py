aae374cd3bf031801800c5054c2cc2a1c24ce1ad
Alex202118020212 <1328350647@qq.com>
2026-05-05 22:23:47 +0800

    Update model_CNN.py

--- Multimodal_Emotion_Recognition/train/model_CNN.py
+++ Multimodal_Emotion_Recognition/train/model_CNN.py
@@ -29,11 +29,7 @@ def validate(model, dataset, batch_size):
 
 # 我们通过继承Dataset类来创建我们自己的数据加载类，命名为FaceDataset
 class FaceDataset(data.Dataset):
-    '''
-    首先要做的是类的初始化。之前的image-emotion对照表已经创建完毕，
-    在加载数据时需用到其中的信息。因此在初始化过程中，我们需要完成对image-emotion对照表中数据的读取工作。
-    通过pandas库读取数据，随后将读取到的数据放入list或numpy中，方便后期索引。
-    '''
+  
     # 初始化
     def __init__(self, root):
         super(FaceDataset, self).__init__()
@@ -43,16 +39,7 @@ class FaceDataset(data.Dataset):
         self.path = np.array(df_path)[:, 0]
         self.label = np.array(df_label)[:, 0]
 
-    '''
-    接着就要重写getitem()函数了，该函数的功能是加载数据。
-    在前面的初始化部分，我们已经获取了所有图片的地址，在这个函数中，我们就要通过地址来读取数据。
-    由于是读取图片数据，因此仍然借助opencv库。
-    需要注意的是，之前可视化数据部分将像素值恢复为人脸图片并保存，得到的是3通道的灰色图（每个通道都完全一样），
-    而在这里我们只需要用到单通道，因此在图片读取过程中，即使原图本来就是灰色的，但我们还是要加入参数从cv2.COLOR_BGR2GARY，
-    保证读出来的数据是单通道的。读取出来之后，可以考虑进行一些基本的图像处理操作，
-    如通过高斯模糊降噪、通过直方图均衡化来增强图像等（经试验证明，在本项目中，直方图均衡化并没有什么卵用，而高斯降噪甚至会降低正确率，可能是因为图片分辨率本来就较低，模糊后基本上什么都看不清了吧）。
-    读出的数据是48X48的，而后续卷积神经网络中nn.Conv2d() API所接受的数据格式是(batch_size, channel, width, higth)，本次图片通道为1，因此我们要将48X48 reshape为1X48X48。
-    '''
+   
 
     # 读取某幅图片，item为索引号
     def __getitem__(self, item):
@@ -72,10 +59,8 @@ class FaceDataset(data.Dataset):
         return face_tensor, label
 
 
-    '''
-    最后就是重写len()函数获取数据集大小了。
-    self.path中存储着所有的图片名，获取self.path第一维的大小，即为数据集的大小。
-    '''
+
+
     # 获取数据集样本个数
     def __len__(self):
         return self.path.shape[0]
